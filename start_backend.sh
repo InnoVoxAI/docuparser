30dce0a9e50a399438bac0e488e6ccc0c060947c
Cesar Medina <csmedina@gmail.com>
2026-07-24 03:21:20 +0000

    refactoring langextract

--- start_backend.sh
+++ start_backend.sh
@@ -11,8 +11,18 @@ fi
 # 3. Configura os caminhos do Python
 export PYTHONPATH="/docuparser/docuparse-project/contracts:/docuparser/docuparse-project/shared"
 
-# 4. Entra no diretório onde o comando uvicorn precisa rodar
-cd /docuparser/docuparse-project/backend-com
-
 # 5. Executa o backend-com
+cd /docuparser/docuparse-project/backend-com
 uv run uvicorn api.app:app --host 0.0.0.0 --port 8070
+
+# 6. Executa o backend-core
+cd /docuparser/docuparse-project/backend-core
+uv run manage.py migrate && uv run manage.py seed_data && uv run manage.py runserver 0.0.0.0:8000
+
+# 7. Executa o backend-ocr
+cd /docuparser/docuparse-project/backend-ocr
+uv run uvicorn api.app:app --host 0.0.0.0 --port 8080
+
+# 8. Executa o langextract-service
+cd /docuparser/docuparse-project/langextract-service
+uv run uvicorn api.app:app --host 0.0.0.0 --port 8091
